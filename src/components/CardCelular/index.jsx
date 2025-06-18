180414812578f4e34dd8b9a52e864cb0a9da9607
Luckxyor <lucaseialben2@gmail.com>
2025-06-18 08:27:49 -0300

    .

--- src/components/CardCelular/index.jsx
+++ src/components/CardCelular/index.jsx
@@ -3,14 +3,14 @@ import "./card-celular.css";
 
 const CardCelular = (celular) => {
 return (
-            <div key={celular.id} className="tarjeta-producto">
-                <Link to={`/producto/${celular.id}`} className="enlace-producto">
-                <img src={celular.fotos[0]} alt={celular.nombre} className="imagen-producto" />
-                <h3 className="nombre-producto">{celular.nombre}</h3>
-                </Link>
-                <p className="descripcion-producto">{celular.descripcion}</p>
-                <p className="precio-producto">Precio: ${celular.precio}</p>
-            </div>
+        <div key={celular.id} className="tarjeta-producto">
+            <Link to={`/producto/${celular.id}`} className="enlace-producto">
+            <img src={celular.fotos[0]} alt={celular.nombre} className="imagen-producto" />
+            <h3 className="nombre-producto">{celular.nombre}</h3>
+            </Link>
+            <p className="descripcion-producto">{celular.descripcion}</p>
+            <p className="precio-producto">Precio: ${celular.precio}</p>
+        </div>
     )
 }
 
