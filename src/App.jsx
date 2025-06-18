0c74d7dba60accc925bcd0787afae750df728fbc
Luckxyor <lucaseialben2@gmail.com>
2025-06-18 08:39:16 -0300

    Porfin funciona github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
 import QuienesSomos from './pages/QuienesSomos';
@@ -9,7 +9,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home/>}/>
@@ -20,7 +20,7 @@ function App() {
           <Route path="/contacto" element={<Contacto />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
